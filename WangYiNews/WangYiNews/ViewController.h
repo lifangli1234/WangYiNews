ff636cb26c23ad58b1b6a4f3dc8ca091a7ee1848
lifangli1234 <919186053@qq.com>
2015-10-19 09:50:02 +0800

    no message

---
+++ WangYiNews/WangYiNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WangYiNews
+//
+//  Created by lifangli on 15/10/19.
+//  Copyright (c) 2015年 lifangli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WangYiNews/WangYiNewsTests/WangYiNewsTests.m
@@ -0,0 +1,40 @@
+//
+//  WangYiNewsTests.m
+//  WangYiNewsTests
+//
+//  Created by lifangli on 15/10/19.
+//  Copyright (c) 2015年 lifangli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WangYiNewsTests : XCTestCase
+
+@end
+
+@implementation WangYiNewsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
