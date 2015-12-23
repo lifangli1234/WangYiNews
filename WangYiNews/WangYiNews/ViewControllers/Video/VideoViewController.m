492675b9b76770a8244be40beb844042a31bd8c1
lifangli1234 <919186053@qq.com>
2015-12-23 18:10:44 +0800

    no message

--- WangYiNews/WangYiNews/ViewControllers/Video/VideoViewController.m
+++ WangYiNews/WangYiNews/ViewControllers/Video/VideoViewController.m
@@ -59,10 +59,6 @@
     
     [self createNavigation];
     [self createContentScrollView];
-    [_videoTableView addHeaderWithTarget:self action:@selector(loadVideoData)];
-    [_videoTableView addFooterWithTarget:self action:@selector(loadMoreVideoData)];
-    [_audioTableView addHeaderWithTarget:self action:@selector(loadAudioData)];
-    [_audioTableView addFooterWithTarget:self action:@selector(loadMoreAudioData)];
     
     self.update = YES;
     count = 1;
@@ -98,23 +94,18 @@
     [self loadDataWithType:1 url:AUDIOSUBURL tableView:_audioTableView];
 }
 
--(void)loadMoreAudioData
-{
-    [self loadDataWithType:2 url:AUDIOSUBURL tableView:_audioTableView];
-}
-
 -(void)loadDataWithType:(NSInteger)type url:(NSString *)url tableView:(UITableView *)tableView
 {
     [[[NetworkTools sharedNetworkTools] GET:url parameters:nil success:^(NSURLSessionDataTask *task, NSDictionary* responseObject) {
         if (tableView == _videoTableView) {
             VideoModel *videoModel = [VideoModel objectWithKeyValues:responseObject];
             [self.titleListArr removeAllObjects];
-            self.titleListArr = videoModel.videoSidList;
+            [self.titleListArr addObjectsFromArray:videoModel.videoSidList];
             _videoTableView.tableHeaderView = nil;
             _videoTableView.tableHeaderView = [self createVideoTableViewHeaderView];
             if (type == 1) {
                 [self.contentListArr removeAllObjects];
-                self.contentListArr = videoModel.videoList;
+                [self.contentListArr addObjectsFromArray:videoModel.videoList];
                 [tableView headerEndRefreshing];
             }else if(type == 2){
                 [self.contentListArr addObjectsFromArray:videoModel.videoList];
@@ -252,37 +243,112 @@
     }];
 }
 
+-(UIView *)createAudioTableViewHeaderView
+{
+    UIView *headerView = [Helper view:DAYBACKGROUNDCOLOR nightColor:NIGHTBACKGROUNDCOLOR];
+    headerView.frame = CGRectMake(0, 0, [Helper screenWidth], 49);
+    
+    [self addContentForAudioHeaderView:headerView index:0 text:@"我的下载" image:@"audionews_indexheader_download@2x"];
+    [self addContentForAudioHeaderView:headerView index:1 text:@"最近播放" image:@"audionews_indexheader_recent@2x"];
+    
+    UIView *line = [Helper view:LINECOLOR nightColor:NIGHTLINECOLOR];
+    line.layer.borderColor = [Helper isNightMode]?NIGHTGRAYCOLOR.CGColor:GRAYCOLOR.CGColor;
+    line.layer.borderWidth = 1;
+    [headerView addSubview:line];
+    [line mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.offset(5);
+        make.left.right.bottom.equalTo(headerView);
+    }];
+    
+    return headerView;
+}
+
+-(void)addContentForAudioHeaderView:(UIView *)superView index:(NSInteger)index text:(NSString *)text image:(NSString *)image
+{
+    CGFloat width = ([Helper screenWidth]-1)/2;
+    
+    UIView *backView = [Helper view:DAYBACKGROUNDCOLOR nightColor:NIGHTBACKGROUNDCOLOR];
+    [superView addSubview:backView];
+    [backView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(superView);
+        make.bottom.equalTo(superView).offset(-5);
+        make.left.equalTo(superView).offset((width+1)*index);
+        make.width.offset(width);
+    }];
+    
+    UILabel *lab = [Helper label:text font:[UIFont systemFontOfSize:13] textColor:[UIColor darkGrayColor] nightTextColor:[UIColor lightGrayColor] textAligment:NSTextAlignmentCenter];
+    [backView addSubview:lab];
+    [lab mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.bottom.equalTo(backView);
+        make.centerX.equalTo(backView).offset(index==0?11:14);
+    }];
+    
+    UIImageView *img = [Helper imageView:image];
+    [backView addSubview:img];
+    [img mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(lab.mas_left).offset(-8);
+        make.centerY.equalTo(backView);
+        make.size.sizeOffset(index==0?CGSizeMake(14, 19):CGSizeMake(20, 20));
+    }];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor clearColor];
+    btn.tag = VIDEOTITLEVIEWBUTTON_TAG+index;
+    [btn addTarget:self action:@selector(videoCatergory) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:btn];
+    [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(backView);
+    }];
+    
+    UIView *line = [Helper view:GRAYCOLOR nightColor:NIGHTGRAYCOLOR];
+    [superView addSubview:line];
+    [line mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(backView.mas_right);
+        make.width.offset(1);
+        make.top.bottom.equalTo(superView);
+    }];
+}
+
 -(void)createVideoTableView
 {
-    _videoTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [Helper screenWidth], [Helper screenHeight]-64)];
+    _videoTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [Helper screenWidth], [Helper screenHeight]-64-49)];
     _videoTableView.delegate = self;
     _videoTableView.dataSource = self;
     _videoTableView.dk_backgroundColorPicker = DKColorWithColors([UIColor whiteColor], NIGHTBACKGROUNDCOLOR);
     _videoTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [_contentScr addSubview:_videoTableView];
+    
+    [_videoTableView addHeaderWithTarget:self action:@selector(loadVideoData)];
+    [_videoTableView addFooterWithTarget:self action:@selector(loadMoreVideoData)];
 }
 
 -(void)createAudioTableView
 {
-    _audioTableView = [[UITableView alloc] initWithFrame:CGRectMake([Helper screenWidth], 0, [Helper screenWidth], [Helper screenHeight]-64)];
+    _audioTableView = [[UITableView alloc] initWithFrame:CGRectMake([Helper screenWidth], 0, [Helper screenWidth], [Helper screenHeight]-64-49)];
     _audioTableView.delegate = self;
     _audioTableView.dataSource = self;
     _audioTableView.dk_backgroundColorPicker = DKColorWithColors([UIColor whiteColor], NIGHTBACKGROUNDCOLOR);
     _audioTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    _audioTableView.tableHeaderView = [self createAudioTableViewHeaderView];
     [_contentScr addSubview:_audioTableView];
+
+    [_audioTableView addHeaderWithTarget:self action:@selector(loadAudioData)];
 }
 
 -(void)createContentScrollView
 {
     _contentScr= [[UIScrollView alloc] init];
     _contentScr.dk_backgroundColorPicker = DKColorWithColors([UIColor grayColor], NIGHTBACKGROUNDCOLOR);
-    _contentScr.contentSize = CGSizeMake([Helper screenWidth]*2, [Helper screenHeight]-64);
+    _contentScr.contentSize = CGSizeMake([Helper screenWidth]*2, [Helper screenHeight]-64-49);
     _contentScr.pagingEnabled = YES;
     _contentScr.showsHorizontalScrollIndicator = NO;
+    _contentScr.showsVerticalScrollIndicator = NO;
+    _contentScr.bounces = NO;
     [self.view addSubview:_contentScr];
     [_contentScr mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view).offset(64);
-        make.bottom.left.right.equalTo(self.view);
+        make.left.right.equalTo(self.view);
+        make.bottom.equalTo(self.view).offset(-49);
     }];
     
     [self createVideoTableView];
